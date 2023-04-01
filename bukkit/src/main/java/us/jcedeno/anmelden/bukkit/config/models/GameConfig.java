af8874ffa168232a7613685abba04c7f12eee9b4
Juan Cedeno <jcedeno@panicsoda.com>
2023-04-01 08:47:28 -0400

    feat: added scenarios, restorable iface & gamecfg

--- bukkit/src/main/java/us/jcedeno/anmelden/bukkit/config/models/GameConfig.java
+++ bukkit/src/main/java/us/jcedeno/anmelden/bukkit/config/models/GameConfig.java
@@ -1,21 +1,44 @@
 package us.jcedeno.anmelden.bukkit.config.models;
 
+import java.util.List;
 import java.util.UUID;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 /**
  * A dataclass to represent a UHC Game Configuration. This is used to store and
  * restore game configurations. Serializable to JSON.
  * 
+ * TODO: Consider saving InitialGameConfig at second 0, then blocking certain
+ * changes to certain config as time goes along and only serialize the object to
+ * save it for failures when a change is made to any of the values in this
+ * object. Use timestamp in restorable json object for stored state.
+ * 
  * @author thejcedeno
  */
 @Data
 public class GameConfig {
     private UUID hostId;
+    private UUID internalTrackingId;
     private String matchName;
 
+    // TODO: Use dedidcated object for this later
+    private List<String> whitelist; // List of UUIDs or offlineplayer names.
+
+    // Game Settings
+    private List<String> scenarios;
+    private boolean nether;
+    private boolean end;
+    private boolean pvp;
+
     private int maxPlayers;
+
+    // Game Timings stuff.
+    private Long pvptime = 15L * 60 * 1000;
+    private Long meetupTime = 60L * 60 * 1000;
+
+    // Timings stuff
+    private long expectedEndTime;
+    private long expectedStartTime;
 
 }
---
+++ bukkit/src/main/java/us/jcedeno/anmelden/bukkit/interfaces/RestorableState.java
@@ -0,0 +1,29 @@
+package us.jcedeno.anmelden.bukkit.interfaces;
+
+/**
+ * This interface is used to allow objects to be snapshotted and restored in case
+ * of a sudden shutdown or backup attempt.
+ * 
+ * @param <T> The object to snapshot
+ */
+public interface RestorableState<T> {
+    /**
+     * Takes a snapshot of the current state of the object, and the implementation
+     * is expected to serialized it to JSON and return it as a string. This method
+     * will be caled in case of a sudden shutdown request or backup attempt.
+     * 
+     * @param t The object to snapshot
+     * 
+     * @return The JSON string of the snapshot.
+     */
+    public String snapshot(T t);
+
+    /**
+     * Restores the object to the state it was in when the snapshot was taken. This
+     * method will be called in case of a sudden shutdown request or backup attempt.
+     * 
+     * @param t The object to restore
+     * @param json The JSON string of the snapshot
+     */
+    public void restore(T t, String json);
+}
